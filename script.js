79b72862f406711ec8346ea96b07fd103065681e
visnhnutvm <vishnuvichu8003@gmail.com>
2021-05-25 21:39:42 +0530

    Enter Data in iput feald and list it from localstorage width edit ,delet,

--- script.js
+++ script.js
@@ -1,22 +1,85 @@
+let kichenInput = document.querySelector("#kichen-input");
+let kichenInputBtn = document.querySelector("#add-btn");
+let kichenInputList = document.querySelector("#item-list");
 
-let input = document.querySelector(".input")
-let stud = document.querySelector(".students");
-input.style.color="red"
-let arr=[];
-let i=0;
+let kichenInputData;
+let kichenInputArry = [];
 
-function clicked(){
+kichenInputBtn.addEventListener("click", addKichenItem);
 
-    if(input.value !=""){
-    
-    arr[i]=input.value;
-  
-           let li=document.createElement("li");
-    stud.appendChild(li)
-          li.innerText=`(${i+1}) ${arr[i]}`
-           i++; 
-    }
-    console.log(arr)
-     input.value="";
+function setLocalStorage() {
+  localStorage.setItem("key", JSON.stringify(kichenInputArry));
+}
+function getLocalStorage() {
+  if (localStorage.getItem("key")) {
+    kichenInputArry = JSON.parse(localStorage.getItem("key"));
+
+    buldUi();
+  }
 }
 
+function buldUi() {
+  kichenInputList.textContent = "";
+  kichenInputArry.forEach((item) => {
+    if (item !== "") {
+      let li = document.createElement("li");
+      let span = document.createElement("span");
+      kichenInputList.appendChild(li);
+      li.style.cssText = "animation-name: slideIn;";
+      li.appendChild(span);
+      span.innerText = item;
+      kichenInput.value = "";
+      kichenInput.focus();
+
+      // creat delet icon
+      let dlt = document.createElement("i");
+      dlt.classList.add("fas", "fa-trash");
+      li.appendChild(dlt);
+
+      // ceat edit icon
+      let edit = document.createElement("i");
+      edit.classList.add("fas", "fa-edit");
+      li.appendChild(edit);
+    }
+  });
+}
+function addKichenItem() {
+  kichenInputData = kichenInput.value;
+  kichenInputArry.push(kichenInputData);
+
+  // local sctorage
+
+  setLocalStorage();
+  getLocalStorage();
+}
+function deleteItem(event) {
+  if (event.target.classList[1] === "fa-trash") {
+    let parrent = event.target.parentElement;
+    let span = parrent.querySelector("span");
+    let oldValue = span.innerText;
+    parrent.classList.add("slideOut");
+    parrent.addEventListener("transitionend", () => {
+      parrent.remove();
+      setLocalStorage();
+    });
+    let position = kichenInputArry.indexOf(oldValue);
+    console.log(position);
+    kichenInputArry.splice(position, 1);
+  }
+}
+function editItem(event) {
+  if (event.target.classList[1] === "fa-edit") {
+    let item = event.target.parentElement;
+    let span = item.querySelector("span");
+    let oldValue = span.innerText;
+    let position = kichenInputArry.indexOf(oldValue);
+    let editedvalue = prompt("Plees Enter New Item...");
+    kichenInputArry[position] = editedvalue;
+    span.innerText = editedvalue;
+    setLocalStorage();
+  }
+}
+
+kichenInputList.addEventListener("click", deleteItem);
+kichenInputList.addEventListener("click", editItem);
+getLocalStorage();
